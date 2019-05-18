05bd3509fd2884198591a56a276ff10def705898
yusansuione <yusansuione@163.com>
2019-05-18 17:28:11 +0800

    Revert "第四次"
    This reverts commit ef273a7

---
+++ src/main/java/com/itheima/demo/demo4.java
@@ -0,0 +1,7 @@
+package com.itheima.demo;
+
+public class demo4 {
+    public static void main(String[] args) {
+        System.out.println("烦躁!");
+    }
+}
---
+++ src/main/java/com/itheima/demo/demo7.java
@@ -0,0 +1,7 @@
+package com.itheima.demo;
+
+public class demo7 {
+    public static void main(String[] args) {
+        System.out.println("777777777");
+    }
+}
